14d18fe070eab9177aa5b383922d46a98d359682
Colin Yip <colinsyyip@gmail.com>
2024-03-19 00:38:20 +0100

    Added config page

--- soundboard_ui.py
+++ soundboard_ui.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import json
 from playback import Soundboard
 
 
@@ -20,6 +21,39 @@ def soundboard_ui():
 
 @app.route("/configs")
 def soundboard_configs():
+    return render_template("sb_configs.html")
+
+
+@app.route("/push_config", methods=["POST"])
+def push_config():
+    form_data = request.form
+    video_url = form_data.get("vid_url")
+    sb_name = form_data.get("sb_name")
+    video_ts = form_data.get("vid_ts")
+    sb_length = form_data.get("sb_length")
+    sb_volume = form_data.get("sb_volume")
+
+    new_conf_dict = {
+        "url": video_url,
+        "ts": float(video_ts),
+        "clip_duration": float(sb_length),
+        "name": sb_name,
+        "volume": float(sb_volume)
+    }
+    new_sb_id = sb_name.lower().replace(" ", "_")
+
+    config_map_file_path = "soundbite_map.json"
+    with open(config_map_file_path, "r") as f:
+        config_file = f.read()
+        f.close()
+    config_dict = json.loads(config_file)
+    config_dict[new_sb_id] = new_conf_dict
+    with open(config_map_file_path, "w") as f:
+        f.write(json.dumps(config_dict))
+        f.close()
+    app.soundboard.soundbite_download(soundbite_name = new_sb_id,
+                                      soundbite_info = new_conf_dict)
+    app.soundboard.load_configs()
     return render_template("sb_configs.html")
 
 
